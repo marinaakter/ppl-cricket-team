3d4673df83ac814805a116731c4cfc5084ea1310
Marina <marinaakter878@gmail.com>
2024-10-25 23:34:04 +0600

    Add Header component and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl text-center text-white font-bold rounded-lg mt-5 p-3 bg-gray-500 w-11/12 mx-auto'
+      <div className='w-11/12 mx-auto'>
+      <h1 className='text-3xl text-center text-white font-bold rounded-lg mt-5 p-3 bg-gray-500 '
       >My BoilerPlate Code</h1>
+      <Header></Header>
+      </div>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <div>
+      <h3>This is marina</h3>
+    </div>
+  )
+}
+
+export default Header
