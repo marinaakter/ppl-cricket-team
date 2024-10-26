dc467076ee7931f18f310a14d9e5cf47b4cdb1ca
Marina <marinaakter878@gmail.com>
2024-10-26 11:33:42 +0600

    Footer added with Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Banner from './components/banner/banner'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 
 function App() {
 
@@ -9,7 +11,9 @@ function App() {
       <div className='w-11/12 mx-auto'>
       <Header></Header>
       <Banner></Banner>
+      <Main ></Main>
       </div>
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+
+const Main = () => {
+  return (
+    <div className="h-96 border border-blue-600">
+        <h2>Marina Akter</h2>
+    </div>
+  )
+}
+
+export default Main
