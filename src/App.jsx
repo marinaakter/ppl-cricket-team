5cac050b41b49b2a3ced2cec86e7869075b97227
Marina <marinaakter878@gmail.com>
2024-10-26 07:03:23 +0600

    Add daisyui and react-icons dependencies and header section done

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ function App() {
   return (
     <>
       <div className='w-11/12 mx-auto'>
-      <h1 className='text-3xl text-center text-white font-bold rounded-lg mt-5 p-3 bg-gray-500 '
-      >My BoilerPlate Code</h1>
       <Header></Header>
       </div>
     </>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,21 @@
-
+import { TbCoinFilled } from "react-icons/tb";
 const Header = () => {
   return (
-    <div>
-      <h3>This is marina</h3>
+    <div className="navbar">
+        <div className="flex-1">
+            <img src="/assets/logo.png" alt="" />
+        </div>
+        <div className="flex-none">
+            <ul className="menu menu-horizontal px-4">
+            <li><a>Link</a></li>
+            <li><a>Fixture</a></li>
+            <li><a>Teams</a></li>
+            <li><a>Schedules</a></li>
+            <li>
+            <button className="border border-gray-200 px-4 ml-4">0 Coin <TbCoinFilled className="text-2xl text-yellow-600"/> </button>
+            </li>
+            </ul>
+        </div>
     </div>
   )
 }
