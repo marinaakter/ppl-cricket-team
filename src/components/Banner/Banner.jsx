dd93945659263566eb76d7ad9f0aeb6c6c6b416c
Marina <marinaakter878@gmail.com>
2024-10-27 20:35:03 +0600

    banner background gradient and text color

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,13 +7,13 @@ const Banner = () => {
     <div
       className="bg-cover bg-no-repeat rounded-3xl h-auto mb-10"
       style={{
-        backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${bgImage})`,
+        backgroundImage: `linear-gradient(to right, rgba(0, 0, 228, 0.1), black, rgba(0, 0, 0, 0.3)), url(${bgImage})`,
       }}
     >
       <div className='w=9/12 mx-auto flex flex-col justify-center items-center text-center h-full space-y-6 py-4'>
       <img className='' src="/assets/banner-main.png" alt="" />
         <h1 className='font-bold text-white md:text-4xl'>Assemble Your Ultimate Dream 11 Cricket Team</h1>
-        <p>Beyond Boundaries Beyond Limits</p>
+        <p className='text-white'>Beyond Boundaries Beyond Limits</p>
         <button className="btn bg-bannerBtn">Claim Free Credit</button>
 
       </div>
